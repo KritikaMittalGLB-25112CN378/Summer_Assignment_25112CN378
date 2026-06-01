c6ed154f0c6925256eb5636fd0ff09075bd45395
Kritika5Mittal <mittalkritika357@gmail.com>
2026-06-01 15:48:49 +0530

    Create Question_1.java

---
+++ Day 1/Question_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Question_1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System .in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=num;i++)
+        {
+          sum+=i;  
+
+        }
+        System.out.print("The sum of the number is: "+ sum);
+
+    }
+}
