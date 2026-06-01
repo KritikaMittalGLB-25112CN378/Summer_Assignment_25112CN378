d65ef043e7fb49a805360079668395db100ac7d7
Kritika5Mittal <mittalkritika357@gmail.com>
2026-06-01 16:42:51 +0530

    Create Question_4.java

---
+++ Day 1/Question_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Question_4 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int dup=num;
+        int count=0;
+        while(dup!=0){
+            int nb=dup%10;
+            count+=1;
+            dup/=10;
+
+        }
+        System.out.print("The number of digits in a number is: "+ count);
+
+    }    
+}
