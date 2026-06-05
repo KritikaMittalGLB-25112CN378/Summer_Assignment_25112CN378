51b7fc38e162463e6d913817dc51c5ba599cf328
Kritika5Mittal <mittalkritika357@gmail.com>
2026-06-05 15:07:18 +0530

    Create Question_20.java

---
+++ Day 5/Question_20.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Question_20 {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int max=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                int count=0;
+                for(int j=1;j<=i;j++){
+                    
+                    if(i%j==0){
+                        count++;
+                    }
+                }
+                if(count==2){
+                    max=i;
+                }
+            }
+        }
+        System.out.print("The maximum prime factor is "+ max);
+    }
+    
+}
