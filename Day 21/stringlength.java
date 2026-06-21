50fe701a12f369963a472f15dd2bed791dfd6c20
Kritika5Mittal <mittalkritika357@gmail.com>
2026-06-21 10:28:18 +0530

    Create stringlength.java

---
+++ Day 21/stringlength.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class stringlength {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str=sc.nextLine();
+
+        int count=0;
+
+        for(char ch : str.toCharArray()){
+            count++;
+        }
+
+
+        System.out.print("Length of the string is "+ count);
+
+        sc.close();
+
+
+    }
+}
