2a3adc48d10f9a0509d49839f3315e402e37a816
Kritika5Mittal <mittalkritika357@gmail.com>
2026-06-23 09:20:18 +0530

    Create firstrepeatingcharacter.java

---
+++ Day 23/firstrepeatingcharacter.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class firstrepeatingcharacter {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String str=sc.nextLine();
+
+        str=str.toLowerCase();
+        boolean found=false;
+        for(int i=0;i<str.length();i++){
+            int count=0;
+            for(int j=0;j<str.length();j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    count++;
+                }
+            }
+
+            if(count>1){
+                System.out.print("The first repeating character in a string is "+ str.charAt(i));
+                found=true;
+                break;
+            }
+
+            if(str.charAt(i)==' '){
+                continue;
+            }
+        }
+        if(!found){
+            System.out.print("There is no first repeating character in a string.");
+        }
+
+        sc.close();
+
+    }
+}
+
+
